3993d0cf9f99d9e425085586ae0a9cdb02eb8c55
Michal Zajac <119435023+MichalZaj@users.noreply.github.com>
2023-11-22 17:14:45 -0600

    Add files via upload
    Chapter 10 files

---
+++ Chapter10/LoxFunction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lox_Interpreter1
+{
+    public class LoxFunction : ILoxCallable
+    {
+        private readonly Stmt.Function declaration;
+        private readonly EnvironmentLox closure;
+        public LoxFunction(Stmt.Function declaration, EnvironmentLox closure)
+        {
+            this.closure = closure;
+            this.declaration = declaration;
+        }
+
+        public object Call(Interpreter interpreter, List<object> arguments)
+        {
+            EnvironmentLox environment = new EnvironmentLox(closure);
+
+            for (int i = 0; i < declaration.Params.Count; i++)
+            {
+                environment.Define(declaration.Params[i].Lexeme, arguments[i]);
+            }
+
+            try
+            {
+                interpreter.ExecuteBlock(declaration.Body, environment);
+            }
+            catch (Return returnValue)
+            {
+                return returnValue.Value;
+            }
+
+            return null;
+        }
+        public int Arity => declaration.Params.Count;
+        public override string ToString()
+        {
+            return $"<fn {declaration.Name.Lexeme}>";
+
+        }
+
+    }
+}
